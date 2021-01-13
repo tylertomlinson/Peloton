b169ca10e895e713cde073f613faf11fd8daa30a
Tyler <twtomlinson89@gmail.com>
2021-01-13 14:10:44 -0700

    Rides to DB

---
+++ db/migrate/20210113201823_create_rides.rb
@@ -0,0 +1,13 @@
+class CreateRides < ActiveRecord::Migration[6.0]
+  def change
+    create_table :rides do |t|
+      t.datetime :date
+      t.text :duration
+      t.text :distance
+      t.text :average_speed
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_220312) do
+ActiveRecord::Schema.define(version: 2021_01_13_201823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rides", force: :cascade do |t|
+    t.datetime "date"
+    t.text "duration"
+    t.text "distance"
+    t.text "average_speed"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_rides_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_01_04_220312) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rides", "users"
 end
