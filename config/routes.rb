0e62db534ce461a9c073e105f8471b589ff729a1
Tyler <twtomlinson89@gmail.com>
2021-01-22 16:29:48 -0700

    Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   post '/users/:id/rides', to: 'rides#create'
   get '/users/:id/rides/new', to: 'rides#new'
+
   resources :rides
+
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  get '/dashboard', to: "dashboard#index"
+
   root 'pages#home'
-
-
 end
